4af471210265a15cae3dd3c733914a314ff5a838
rinoto <ruben_inoto@yahoo.com>
2015-09-13 13:40:24 +0200

    #5 test to check that ApplicationContext can be autowired

---
+++ src/test/java/sbg/rinoto/spring/mock/appcontext/AppContextAutowireTest.java
@@ -0,0 +1,28 @@
+package sbg.rinoto.spring.mock.appcontext;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static sbg.rinoto.spring.mock.matcher.IsImplementation.isImplementation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import sbg.rinoto.spring.mock.AutoMockRegistryPostProcessor;
+import sbg.rinoto.spring.mock.classes.ClassToTest;
+
+@ContextConfiguration(classes = { AutoMockRegistryPostProcessor.class, ClassToTest.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AppContextAutowireTest {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void shouldNotMockAppContext() throws Exception {
+		assertThat(applicationContext, isImplementation());
+	}
+
+}
